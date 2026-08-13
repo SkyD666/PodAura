2a8be673184012109ac9c1098c71b4bd60d9c685
SkyD666 <akarinnn666@gmail.com>
2026-08-13 23:03:43 +0800

    [optimize] Show the article list refresh button by default on desktop

--- shared/src/commonMain/kotlin/com/skyd/podaura/model/preference/appearance/article/ShowArticleTopBarRefreshPreference.kt
+++ shared/src/commonMain/kotlin/com/skyd/podaura/model/preference/appearance/article/ShowArticleTopBarRefreshPreference.kt
@@ -1,6 +1,8 @@
 package com.skyd.podaura.model.preference.appearance.article
 
 import androidx.datastore.preferences.core.booleanPreferencesKey
+import com.skyd.fundation.util.isPhone
+import com.skyd.fundation.util.platform
 import com.skyd.ksp.annotation.Preference
 import com.skyd.podaura.model.preference.BasePreference
 
@@ -8,6 +10,6 @@ import com.skyd.podaura.model.preference.BasePreference
 object ShowArticleTopBarRefreshPreference : BasePreference<Boolean>() {
     private const val SHOW_ARTICLE_TOP_BAR_REFRESH = "showArticleTopBarRefresh"
 
-    override val default = false
+    override val default = !platform.isPhone
     override val key = booleanPreferencesKey(SHOW_ARTICLE_TOP_BAR_REFRESH)
 }
